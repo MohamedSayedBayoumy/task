5960c8a7f809dde3c6a8fb4e06d80d2d8f571a5a
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2023-11-29 05:32:08 +0200

    Add New vaildation ..

--- lib/common/functions/validation/vailda.dart
+++ lib/common/functions/validation/vailda.dart
@@ -4,14 +4,18 @@ abstract class Validation {
   }
 
   static emailFormat(String value) {
-    if (value.isEmpty) return "This Filed is empty";
+    isEmpty(value);
     if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
       return 'You should enter valid email';
+    }
+    if (!value.endsWith('.com')) {
+      return 'Invalid email , Must end with ".com"';
     }
     return null;
   }
 
-   static checkConformation({required String value  , required String compareWith}) {
+  static checkConformation(
+      {required String value, required String compareWith}) {
     if (value.isEmpty) return "This Filed is empty";
     if (value != compareWith) {
       return 'Invaild Password';
